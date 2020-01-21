4f6b04d49f3442a0404d875c4d5253920dea32ec
Anderson Coelho <axzonexgamer@gmail.com>
2020-01-21 12:58:01 +0000

    moved routes to do with the riot games league api into its own route file, this marks the end of the current refactoring phase.

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 const environment = require('./environment')
 const app = environment.config()
-const fetch = require('node-fetch')
 const mongoose = require('mongoose')
+const summonerDataRoutes = require('./routes/summonerData')
 const port = 3001
-const {getChampionReferences, synchronizePatchVersion,endpoints,appendChampionData} = require('./apis/lol/functions')
-const { summonerProfile,summonerCM, APIAuth, loadingScreenImg, profileIcon,championImg } = endpoints
 
-const main = async () => {
-    let version = await synchronizePatchVersion()
-    let championsList = await getChampionReferences(version)
+app.use('/summonerdata', summonerDataRoutes)
+app.listen(port)
+console.log(`online on port ${port}`)
+    
 
-    app.post('/', (req,res) => {
-        const { name } = req.body
-        const url = `${summonerProfile}/${name}${APIAuth}`;
-        fetch(url)
-        .then(resp => resp.json())
-        .then(profile => {
-            return fetch(summonerCM+profile.id+APIAuth)
-            .then(resp => resp.json())
-            .then(CMObject => {
-                // console.log(CMObject)
-                CMObject = Object.values(CMObject).map(champion => appendChampionData(version,championsList,champion))
-                profile.CMList = CMObject
-                profile.profileIconId = profileIcon(version)+profile.profileIconId+".png"
-                return profile
-            })
-            .then(() => profile)
-        })
-        .then(profile => res.json(profile))
-        .catch(err => res.json(err.message))
-    })
-   
-    app.listen(port)
-    console.log(`online on port ${port}`)
-    console.log(`Patch version  ${version}`)
-}
 mongoose.connect('mongodb://127.0.0.1:27017/lolfl',{ useNewUrlParser: true,  useUnifiedTopology: true,useFindAndModify: false  }, ()=> {
     console.log('connected!')
-    // version =  synchronizePatchVersion() 
-})
 
-main()
+})
