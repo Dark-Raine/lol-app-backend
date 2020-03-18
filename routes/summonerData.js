18806cbd8f80266ac008ce007293c1f67885180e
Anderson Coelho <axzonexgamer@gmail.com>
2020-03-18 20:16:09 +0000

    utilising profilerestructure to make the controller simpler

--- routes/summonerData.js
+++ routes/summonerData.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { endpoints, getRequestParser, profileRestructure, verifySummonerCode, urlBuilder } = require('../apis/lol/functions')
+const { endpoints, getRequestParser, profileRestructure, verifySummonerCode, urlBuilder, apiResponseChecker } = require('../apis/lol/functions')
 const { summonerProfile, verification, verifyAccount } = endpoints
     
-    router.post('/', (req,res) => {
+    router.post('/', async function getdata(req,res)  {
         const { name } = req.body
         const url = urlBuilder(summonerProfile,name)
-        getRequestParser(url)
-        .then(profileRestructure)
-        .then(profile => res.json(profile))
-        .catch(err => res.json(err.message))
+        const parsedData = await getRequestParser(url)
+        apiResponseChecker(parsedData,res,profileRestructure)
     })
     
     router.get('/verification', (req,res)=>{
@@ -22,8 +20,6 @@ const { summonerProfile, verification, verifyAccount } = endpoints
         const summonerCode = await getRequestParser(url)
         const verified = await verifySummonerCode(summonerCode)
         verified ? res.status(200).json({verified}) : res.status(404).json({verified})
-        
-        // .catch(err => res.json({error: err.message}))
     })
 
     module.exports = router
